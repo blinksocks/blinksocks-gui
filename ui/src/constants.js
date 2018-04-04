b59e8d966c1d5fb34ddd41ddf8deee59e6d0b0cd
Micooz <micooz@hotmail.com>
2018-04-04 15:13:15 +0800

    ui: enlarge RPC_TIMEOUT to 30s

--- ui/src/constants.js
+++ ui/src/constants.js
@@ -6,7 +6,7 @@ export const HASH_SALT = 'blinksocks';
 
 // this value must be less than server dispose timeout
 export const KEEPALIVE_INTERVAL = 5e4; // 50s
-export const RPC_TIMEOUT = 1e4; // 10s
+export const RPC_TIMEOUT = 3e4; // 30s
 
 export const RPC_STATUS_INIT = -1;
 export const RPC_STATUS_ONLINE = 0;

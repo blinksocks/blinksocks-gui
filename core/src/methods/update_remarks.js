f6ec63f444bdfa90ab63a3002cd187b56220bedf
Micooz <micooz@hotmail.com>
2018-04-04 16:09:49 +0800

    core: check remarks when update_remarks()

--- core/src/methods/update_remarks.js
+++ core/src/methods/update_remarks.js
@@ -1,3 +1,6 @@
 module.exports = async function update_remarks({ id, remarks }) {
+  if (typeof remarks !== 'string' || remarks.length < 1) {
+    throw Error('invalid parameter');
+  }
   this.db.getConfigs().find({ id }).assign({ remarks }).write();
 };

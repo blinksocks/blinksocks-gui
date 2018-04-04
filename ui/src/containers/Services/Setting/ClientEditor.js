7f0515ad161ed8b1aba649fffa2fa6106511fd04
Micooz <micooz@hotmail.com>
2018-04-04 21:56:31 +0800

    ui: refactor service <Setting>

--- ui/src/containers/Services/Setting/ClientEditor.js
+++ ui/src/containers/Services/Setting/ClientEditor.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import _ from 'lodash';
+import classnames from 'classnames';
+import { TagInput } from '@blueprintjs/core';
 
-import AddressEditor from './components/AddressEditor';
-import CommonEditor from './components/CommonEditor';
+import AddressEditor from './AddressEditor/AddressEditor';
+
 import styles from './ClientEditor.module.css';
 
 export default class ClientEditor extends React.Component {
@@ -18,55 +19,149 @@ export default class ClientEditor extends React.Component {
     },
   };
 
-  state = {
-    service: '',
-    commons: {},
-  };
-
-  constructor(props) {
-    super(props);
-    const { service } = this.props.client;
-    this.state = {
-      service,
-      commons: _.pick(this.props.client, [
-        'dns', 'dns_expire', 'timeout',
-        'log_path', 'log_level', 'log_max_days',
-      ]),
-    };
+  static getDerivedStateFromProps({ client }) {
+    return client;
   }
 
+  state = {
+    service: '',
+    dns: [],
+    dns_expire: 3600,
+    timeout: 200,
+    log_path: '',
+    log_level: '',
+    log_max_days: 30,
+  };
+
+  _isAdvancedShow = false;
+
   onChange = () => {
-    const { service, commons } = this.state;
-    this.props.onChange({
-      service: service,
-      ...commons,
-    });
+    this.props.onChange(this.state);
+  };
+
+  onToggleAdvanced = () => {
+    this._isAdvancedShow = !this._isAdvancedShow;
+    this.forceUpdate();
   };
 
   onAddressChange = (address) => {
     this.setState({ service: address }, this.onChange);
   };
 
-  onCommonsChange = (commons) => {
-    this.setState({ commons }, this.onChange);
+  onLogPathChange = (e) => {
+    this.setState({ log_path: e.target.value }, this.onChange);
+  };
+
+  onLogLevelChange = (e) => {
+    this.setState({ log_level: e.target.value }, this.onChange);
+  };
+
+  onLogMaxDaysChange = (e) => {
+    this.setState({ log_max_days: e.target.value | 0 }, this.onChange);
+  };
+
+  onTimeoutChange = (e) => {
+    this.setState({ timeout: e.target.value | 0 }, this.onChange);
+  };
+
+  onDnsChange = (servers) => {
+    this.setState({ dns: servers }, this.onChange);
+  };
+
+  onDnsExpireChange = (e) => {
+    this.setState({ dns_expire: e.target.value | 0 }, this.onChange);
   };
 
   render() {
-    const { service, commons } = this.state;
+    const { service, log_path, log_level, log_max_days } = this.state;
+    const { timeout, dns, dns_expire } = this.state;
     return (
       <div className={styles.container}>
-        <div className={styles.subitem}>
+        <section>
           <h5>Address</h5>
           <AddressEditor
             address={service}
             protocols={['socks5', 'socks4', 'socks4a', 'http']}
             onChange={this.onAddressChange}
           />
-        </div>
-        <CommonEditor
-          commons={commons}
-          onChange={this.onCommonsChange}
-        />
+        </section>
+        <section>
+          <h5>Log Path</h5>
+          <input
+            type="text"
+            className="pt-input"
+            placeholder="log_path"
+            value={log_path}
+            onChange={this.onLogPathChange}
+          />
+        </section>
+        <button
+          className={classnames(
+            'pt-button pt-fill pt-minimal',
+            {
+              'pt-active': this._isAdvancedShow,
+              'pt-icon-chevron-up': this._isAdvancedShow,
+              'pt-icon-chevron-down': !this._isAdvancedShow,
+            },
+            styles.advanced
+          )}
+          onClick={this.onToggleAdvanced}>
+          Advanced
+        </button>
+        {this._isAdvancedShow && (
+          <>
+            <section>
+              <h5>Log Level</h5>
+              <div className="pt-select">
+                <select value={log_level} onChange={this.onLogLevelChange}>
+                  <option disabled defaultValue="">Log Level...</option>
+                  {['error', 'debug', 'verbose', 'warn', 'info', 'silly'].map((level, i) => (
+                    <option key={i} value={level}>{level}</option>
+                  ))}
+                </select>
+              </div>
+            </section>
+            <section>
+              <h5>Log Max Days</h5>
+              <input
+                type="number"
+                className="pt-input"
+                min="1"
+                value={log_max_days}
+                onChange={this.onLogMaxDaysChange}
+              />
+            </section>
+            <section>
+              <h5>Timeout</h5>
+              <input
+                type="number"
+                className="pt-input"
+                placeholder="timeout"
+                value={timeout}
+                onChange={this.onTimeoutChange}
+              />
+            </section>
+            <section>
+              <h5>DNS Servers</h5>
+              <TagInput
+                leftIcon="multi-select"
+                placeholder="dns servers"
+                values={dns}
+                onChange={this.onDnsChange}
+              />
+            </section>
+            <section>
+              <h5>DNS Expire</h5>
+              <input
+                type="number"
+                className="pt-input"
+                min="1"
+                value={dns_expire}
+                onChange={this.onDnsExpireChange}
+              />
+            </section>
+          </>
+        )}
       </div>
     );
   }
--- ui/src/containers/Services/Setting/ServerEditor.js
+++ ui/src/containers/Services/Setting/ServerEditor.js
@@ -2,12 +2,11 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import _ from 'lodash';
 import classnames from 'classnames';
-import { Switch } from '@blueprintjs/core';
+import { Switch, TagInput } from '@blueprintjs/core';
 
-import Tooltip from './components/ToolTip';
-import AddressEditor from './components/AddressEditor';
-import ProtocolStackEditor from './components/ProtocolStackEditor';
-import CommonEditor from './components/CommonEditor';
+import Tooltip from './ToolTip/ToolTip';
+import AddressEditor from './AddressEditor/AddressEditor';
+import ProtocolStackEditor from './ProtocolStatckEditor/ProtocolStackEditor';
 
 import styles from './ServerEditor.module.css';
 
@@ -25,62 +24,53 @@ export default class ServerEditor extends React.Component {
     },
   };
 
+  static getDerivedStateFromProps({ server }) {
+    return {
+      ..._.omit(server, 'key'),
+      secretKey: server.key,
+    };
+  }
+
   state = {
     service: '',
     secretKey: '',
     presets: [],
     mux: false,
-
     // client only
     mux_concurrency: 10,
-
     // server only
     redirect: '',
     tls_key: '',
     tls_cert: '',
     acl: false,
     acl_conf: '',
-    commons: {},
-
-    isShowPassword: false,
+    dns: [],
+    dns_expire: 3600,
+    timeout: 200,
+    log_path: '',
+    log_level: '',
+    log_max_days: 30,
   };
+
+  _isShowPassword = false;
+
+  _isAdvancedShow = false;
 
   $password = null;
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      ...this.props.server,
-      secretKey: this.props.server.key,
-      commons: _.pick(this.props.server, [
-        'dns', 'dns_expire', 'timeout',
-        'log_path', 'log_level', 'log_max_days',
-      ]),
-    };
-  }
-
   onChange = () => {
-    const { isClient } = this.props;
-    const { service, secretKey, presets, mux, mux_concurrency } = this.state;
-    const { commons, redirect, tls_key, tls_cert, acl, acl_conf } = this.state;
-    const config = {
-      service,
-      key: secretKey,
-      presets,
-      mux,
-    };
-    if (isClient) {
-      config['mux_concurrency'] = mux_concurrency;
-    } else {
-      Object.assign(config, commons, {
-        redirect,
-        tls_key,
-        tls_cert,
-        acl,
-        acl_conf,
+    if (this.props.isClient) {
+      const { service, secretKey, presets, mux, mux_concurrency } = this.state;
+      this.props.onChange({
+        service,
+        key: secretKey,
+        presets,
+        mux,
+        mux_concurrency,
       });
+    } else {
+      this.props.onChange(this.state);
     }
-    this.props.onChange(config);
   };
 
   onServiceChange = (address) => {
@@ -100,8 +90,9 @@ export default class ServerEditor extends React.Component {
   };
 
   onTogglePasswordView = () => {
-    this.setState({ isShowPassword: !this.state.isShowPassword });
+    this._isShowPassword = !this._isShowPassword;
     this.$password.focus();
+    this.forceUpdate();
   };
 
   onAddPreset = (preset) => {
@@ -155,50 +146,215 @@ export default class ServerEditor extends React.Component {
     this.setState({ acl_conf: e.target.value }, this.onChange);
   };
 
-  onCommonsChange = (commons) => {
-    this.setState({ commons }, this.onChange);
+  onToggleAdvanced = () => {
+    this._isAdvancedShow = !this._isAdvancedShow;
+    this.forceUpdate();
+  };
+
+  onDnsChange = (servers) => {
+    this.setState({ dns: servers }, this.onChange);
+  };
+
+  onDnsExpireChange = (e) => {
+    this.setState({ dns_expire: e.target.value | 0 }, this.onChange);
+  };
+
+  onTimeoutChange = (e) => {
+    this.setState({ timeout: e.target.value | 0 }, this.onChange);
+  };
+
+  onLogPathChange = (e) => {
+    this.setState({ log_path: e.target.value }, this.onChange);
+  };
+
+  onLogLevelChange = (e) => {
+    this.setState({ log_level: e.target.value }, this.onChange);
+  };
+
+  onLogMaxDaysChange = (e) => {
+    this.setState({ log_max_days: e.target.value | 0 }, this.onChange);
+  };
+
+  // renders
+
+  renderTLSConf = () => {
+    const { isClient } = this.props;
+    const { tls_key, tls_cert } = this.state;
+    return (
+      <>
+        {!isClient && (
+          <section>
+            <h5>TLS Key Name</h5>
+            <input
+              className="pt-input"
+              placeholder="key.pem"
+              value={tls_key}
+              onChange={this.onTLSKeyChange}
+            />
+          </section>
+        )}
+        <section>
+          <h5>TLS Cert Name</h5>
+          <input
+            className="pt-input"
+            placeholder="cert.pem"
+            value={tls_cert}
+            onChange={this.onTLSCertChange}
+          />
+        </section>
+      </>
+    );
+  };
+
+  renderMuxConf = () => {
+    const { isClient } = this.props;
+    const { mux, mux_concurrency } = this.state;
+    return (
+      <>
+        <div className="pt-control-group">
+          <Switch
+            inline
+            className="pt-large"
+            checked={mux}
+            onChange={this.onToggleMux}
+          />
+        </div>
+        {mux && isClient && (
+          <div>
+            <h6>Mux Concurrency</h6>
+            <input
+              type="number"
+              className="pt-input"
+              min="1"
+              placeholder="mux concurrency"
+              value={mux_concurrency}
+              onChange={this.onMuxConcurrencyChange}
+            />
+          </div>
+        )}
+      </>
+    );
+  };
+
+  renderServerSideOnly = () => {
+    const { redirect, acl, acl_conf } = this.state;
+    const { log_path, log_level, log_max_days, dns, dns_expire, timeout } = this.state;
+    return (
+      <>
+        <section>
+          <h5>
+            Redirect
+            <Tooltip content="A redirect target when any preset failed. e.g, example.com:80"/>
+          </h5>
+          <input
+            type="text"
+            className="pt-input"
+            placeholder="redirect"
+            value={redirect}
+            onChange={this.onRedirectChange}
+          />
+        </section>
+        <section>
+          <h5>ACL</h5>
+          <Switch
+            inline
+            className="pt-large"
+            checked={acl}
+            onChange={this.onToggleACL}
+          />
+        </section>
+        {acl && (
+          <section>
+            <h5>ACL Config</h5>
+            <input
+              type="text"
+              className="pt-input"
+              placeholder="acl_conf"
+              value={acl_conf}
+              onChange={this.onACLConfChange}
+            />
+          </section>
+        )}
+        <section>
+          <h5>Log Path</h5>
+          <input
+            type="text"
+            className="pt-input"
+            placeholder="log_path"
+            value={log_path}
+            onChange={this.onLogPathChange}
+          />
+        </section>
+        <section>
+          <h5>Log Level</h5>
+          <div className="pt-select">
+            <select value={log_level} onChange={this.onLogLevelChange}>
+              <option disabled defaultValue="">Log Level...</option>
+              {['error', 'debug', 'verbose', 'warn', 'info', 'silly'].map((level, i) => (
+                <option key={i} value={level}>{level}</option>
+              ))}
+            </select>
+          </div>
+        </section>
+        <section>
+          <h5>Log Max Days</h5>
+          <input
+            type="number"
+            className="pt-input"
+            min="1"
+            value={log_max_days}
+            onChange={this.onLogMaxDaysChange}
+          />
+        </section>
+        <section>
+          <h5>Timeout</h5>
+          <input
+            type="number"
+            className="pt-input"
+            placeholder="timeout"
+            value={timeout}
+            onChange={this.onTimeoutChange}
+          />
+        </section>
+        <section>
+          <h5>DNS Servers</h5>
+          <TagInput
+            leftIcon="multi-select"
+            placeholder="dns servers"
+            values={dns}
+            onChange={this.onDnsChange}
+          />
+        </section>
+        <section>
+          <h5>DNS Expire</h5>
+          <input
+            type="number"
+            className="pt-input"
+            min="1"
+            value={dns_expire}
+            onChange={this.onDnsExpireChange}
+          />
+        </section>
+      </>
+    );
   };
 
   render() {
     const { isClient, defs } = this.props;
-    const { isShowPassword } = this.state;
-    const { service, secretKey, presets, mux, mux_concurrency } = this.state;
-    const { commons, redirect, tls_key, tls_cert, acl, acl_conf } = this.state;
+    const { service, secretKey, presets } = this.state;
+    const isShowPassword = this._isShowPassword;
     return (
       <div className={styles.container}>
-        <div className={styles.subitem}>
+        <section>
           <h5>Address</h5>
           <AddressEditor
             address={service}
             protocols={['tcp', 'ws', 'tls']}
             onChange={this.onServiceChange}
           />
-        </div>
-        {service.startsWith('tls://') && (
-          <>
-            {!isClient && (
-              <div className={styles.subitem}>
-                <h5>TLS Key Name</h5>
-                <input
-                  className="pt-input"
-                  placeholder="key.pem"
-                  value={tls_key}
-                  onChange={this.onTLSKeyChange}
-                />
-              </div>
-            )}
-            <div className={styles.subitem}>
-              <h5>TLS Cert Name</h5>
-              <input
-                className="pt-input"
-                placeholder="cert.pem"
-                value={tls_cert}
-                onChange={this.onTLSCertChange}
-              />
-            </div>
-          </>
-        )}
-        <div className={styles.subitem}>
+        </section>
+        {service.startsWith('tls://') && this.renderTLSConf()}
+        <section>
           <h5>Secret Key<Tooltip content="A master key for encryption and decryption."/></h5>
           <div className="pt-input-group">
             <input
@@ -217,8 +373,8 @@ export default class ServerEditor extends React.Component {
               onClick={this.onTogglePasswordView}
             />
           </div>
-        </div>
-        <div className={styles.subitem}>
+        </section>
+        <section>
           <h5>Protocol Stack<Tooltip content="An ordered preset list which build a specific protocol stack."/></h5>
           <ProtocolStackEditor
             presets={presets}
@@ -229,72 +385,28 @@ export default class ServerEditor extends React.Component {
             onRemovePreset={this.onRemovePreset}
             onSortPresets={this.onSortPresets}
           />
-        </div>
-        <div className={styles.subitem}>
-          <h5>Multiplexing</h5>
-          <div className="pt-control-group">
-            <Switch
-              inline
-              className="pt-large"
-              checked={mux}
-              onChange={this.onToggleMux}
-            />
-          </div>
-          {mux && isClient && (
-            <div>
-              <h6>Mux Concurrency</h6>
-              <input
-                type="number"
-                className="pt-input"
-                min="1"
-                placeholder="mux concurrency"
-                value={mux_concurrency}
-                onChange={this.onMuxConcurrencyChange}
-              />
-            </div>
+        </section>
+        <button
+          className={classnames(
+            'pt-button pt-fill pt-minimal',
+            {
+              'pt-active': this._isAdvancedShow,
+              'pt-icon-chevron-up': this._isAdvancedShow,
+              'pt-icon-chevron-down': !this._isAdvancedShow,
+            },
+            styles.advanced
           )}
-        </div>
-        {!isClient && (
-          <div>
-            <div className={styles.subitem}>
-              <h5>
-                Redirect
-                <Tooltip content="A redirect target when any preset failed. e.g, example.com:80"/>
-              </h5>
-              <input
-                type="text"
-                className="pt-input"
-                placeholder="redirect"
-                value={redirect}
-                onChange={this.onRedirectChange}
-              />
-            </div>
-            <div className={styles.subitem}>
-              <h5>ACL</h5>
-              <Switch
-                inline
-                className="pt-large"
-                checked={acl}
-                onChange={this.onToggleACL}
-              />
-            </div>
-            {acl && (
-              <div className={styles.subitem}>
-                <h5>ACL Config</h5>
-                <input
-                  type="text"
-                  className="pt-input"
-                  placeholder="acl_conf"
-                  value={acl_conf}
-                  onChange={this.onACLConfChange}
-                />
-              </div>
-            )}
-            <CommonEditor
-              commons={commons}
-              onChange={this.onCommonsChange}
-            />
-          </div>
+          onClick={this.onToggleAdvanced}>
+          Advanced
+        </button>
+        {this._isAdvancedShow && (
+          <>
+            <section>
+              <h5>Multiplexing</h5>
+              {this.renderMuxConf()}
+            </section>
+            {!isClient && this.renderServerSideOnly()}
+          </>
         )}
       </div>
     );
